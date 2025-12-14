b8bf6bc006024fa599479cd6fb20659c8bb5c2f6
JoshuaOzibo <joshuamichaelozibo@gmail.com>
2025-12-14 15:35:36 +0100

    samll fix

--- ai_social_media_post_generator_frontend/src/components/footer.tsx
+++ ai_social_media_post_generator_frontend/src/components/footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="w-full mt-auto container mx-auto px-4 py-12 text-center text-slate-600 border-t border-slate-200">
+    <footer className="w-full sm:mt-20 mt-auto container mx-auto px-4 py-12 text-center text-slate-600 border-t border-slate-200">
       <p>&copy; 2024 PostGenius. All rights reserved.</p>
     </footer>
   );
